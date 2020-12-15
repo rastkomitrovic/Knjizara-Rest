d5d13c33e6bf0030d8a2ac8e7ea20189212f8ce2
Rastko Mitrovic <rastkomitrovic@gmail.com>
2020-12-15 23:18:58 +0100

    Commit

--- src/main/kotlin/com/fon/knjizararest/entity/City.kt
+++ src/main/kotlin/com/fon/knjizararest/entity/City.kt
@@ -8,6 +8,5 @@ data class City(
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "city_id", nullable = false) val cityId: Long,
         @Column(name = "city_name", nullable = false) val cityName: String,
         @Column(name = "postal_code", nullable = false) val postalCode: String,
-        @Column(name = "description", nullable = true) val description: String? = "",
-        @ManyToOne @JoinColumn(name = "country_id", nullable = false) val country: Country
+        @Column(name = "description", nullable = true) val description: String? = ""
 )
